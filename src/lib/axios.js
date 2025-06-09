4309c7bb5b0b688bd04cb2331af909dc141546ab
revanthvalluru20 <revanthvalluri20@gmail.com>
2025-06-09 11:25:55 +0530

    added /lib/axios.js

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 //const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api";
-const BASE_URL = import.meta.env.MODE === "development" ? "https://v-connect-chat-call-application.onrender.com" : "/api";
+const BASE_URL = import.meta.env.MODE === "development" ? "https://v-connect-chat-call-application.onrender.com/api" : "/api";
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
